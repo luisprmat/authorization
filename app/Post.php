6bd0f7a097500f0acca38ec99799be05a41780af
Luis Parrado <luisprmat@gmail.com>
2019-12-19 14:40:27 -0500

    Remove user_id of fillable property avoiding a MassAssigment security issue

--- app/Post.php
+++ app/Post.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    protected $fillable = ['title', 'user_id', 'teaser', 'content'];
+    protected $fillable = ['title', 'teaser', 'content'];
 
     public function author()
     {
